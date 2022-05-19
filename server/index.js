f7a059bc5d820707cb27060a78daab29ea9424e4
Munna-Khandakar <munnashisad@gmail.com>
2022-05-19 12:51:25 +0600

    without css

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 8080;
+
+// Databse Connection
+const db_connection = require("./config/database").promise();
+
+app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+
+app.get("/allPost", async (req, res) => {
+  try {
+    const [rows] = await db_connection.execute("SELECT * FROM posts ");
+    return res.json({ success: true, listall: rows });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/addArticle", async (req, res) => {
+  try {
+    const [rows] = await db_connection.execute(
+      "INSERT INTO `posts` (`title`,`description`,`information`) VALUES(?, ?, ?)",
+      [req.body.title, req.body.description, req.body.information]
+    );
+    if (rows.affectedRows === 1) {
+      return res.json({ success: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/getPostId", async (req, res) => {
+  try {
+    const [rows] = await db_connection.execute(
+      "SELECT * FROM  posts where id = ? ",
+      [req.body.ids]
+    );
+    if (rows.length > 0) {
+      return res.json({ success: true, listId: rows });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/editArticle", async (req, res) => {
+  try {
+    const [update] = await db_connection.execute(
+      "UPDATE `posts` SET `title`=?, `description`=?,`information`=? WHERE id = ?",
+      [req.body.title, req.body.description, req.body.information, req.body.ids]
+    );
+    if (update.affectedRows === 1) {
+      return res.json({ success: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router basename="/">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Add" component={Add} />
+          <Route path="/Edit/:postID" component={Edit} />
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
